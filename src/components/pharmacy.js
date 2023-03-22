7b5baf59edb07927f8fe64352b3f6f188ff20ba7
valen <valentino.dicapua@ing.austral.edu.ar>
2023-03-22 11:04:15 -0300

    added pharmacy register

--- src/components/pharmacy.js
+++ src/components/pharmacy.js
@@ -1,13 +1,44 @@
+import { Link, useNavigate } from "react-router-dom"
+import {toast} from "react-toastify";
+import { useState } from "react";
+
+
+
 export const RegisterPharmacy=() =>{
-    return(<div class="register">
-    <form>
-         <div class="input-group">
-         <label for="username">Username</label>
+  const [id, idchange] = useState("");
+  const [username, namechange] = useState("");
+  const [password, passwordchange] = useState("");
+  const [mail, emailchange] = useState("");
+
+
+  const navigate = useNavigate();
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    let regobj = { id, username, password, mail };
+    console.log(regobj)
+    fetch("http://localhost:8000/medic", {
+              method: "POST",
+              headers: { 'content-type': 'application/json' },
+              body: JSON.stringify(regobj)
+          }).then((res) => {
+              toast.success('Registered successfully.')
+              navigate('/login');
+          }).catch((err) => {
+              toast.error('Failed :' + err.message);
+          });
+      }
+
+    return(<div className="register">
+    <form onSubmit={handlesubmit}>
+         <div className="input-group">
+         <label >Username</label>
          <input
               id="username"
               name="username"
               placeholder="Enter Username"
               required="required"
+              value={username} onChange={e => namechange(e.target.value)}
          />
          </div>
 
@@ -18,16 +49,18 @@ export const RegisterPharmacy=() =>{
                    name="pharmacy-enrollment"
                    placeholder="Enter id"
                    required="required"
+                   value={id} onChange={e => idchange(e.target.value)}
               />
          </div>
         <div class="input-group">
           <label for="email">Email</label>
           <input
-            id="email"
+            id="mail"
             type="email"
-            name="email"
+            name="mail"
             placeholder="Enter your email"
             required="required"
+            value={mail} onChange={e => emailchange(e.target.value)}
           />
         </div>
         <div class="input-group">
@@ -38,6 +71,7 @@ export const RegisterPharmacy=() =>{
             name="password"
             placeholder="Enter your password"
             required="required"
+            value={password} onChange={e => passwordchange(e.target.value)}
           />
         </div>
         <div class="submit-group">
